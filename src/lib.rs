2faf9fbdfcdf62e07c16bb58c7843272d45da940
michael <mchlstckl@gmail.com>
2019-02-19 22:02:49 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,39 @@
-use std::path::Path;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashMap;
+use std::path::Path;
+// use std::collections::HashMap;
+use hashbrown::HashMap;
 use std::error::Error;
+use walkdir::WalkDir;
+use rayon::prelude::*;
 
-fn find_words(content: &str) -> HashMap<String, u32> {
+pub fn count_words(path: impl AsRef<Path>) -> Result<Vec<(String, u32)>, Box<dyn Error>> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"\b[\p{L}']+\b").unwrap();
     }
-    let mut word_counts = HashMap::new();
-    for mat in RE.find_iter(content) {
-        let word = &content[mat.start()..mat.end()];
-        let count = word_counts.entry(word.to_owned()).or_insert(0u32);
-        *count += 1;
-    }
-    word_counts
-}
 
-pub fn count_words(path: impl AsRef<Path>) -> Result<Vec<(String, u32)>, Box<dyn Error>> {
-    let content = std::fs::read_to_string(path.as_ref())?;
-    let count_map = find_words(&content);
-    let mut count_vec: Vec<_> = count_map.into_iter().collect();   
+    let walker: Vec<_> = WalkDir::new(path)
+        .follow_links(true)
+        .contents_first(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .collect();
+
+    let mut counts = HashMap::new();
+
+    for entry in walker {
+        if let Ok(content) = std::fs::read_to_string(entry.path()) {
+            for mat in RE.find_iter(&content) {
+                let word = &content[mat.start()..mat.end()];
+                let count = counts.entry(word.to_owned()).or_insert(0u32);
+                *count += 1;
+            }
+        }
+    }
+
+    let mut count_vec: Vec<_> = counts.into_iter().collect();
     count_vec.sort_by(|a, b| a.1.cmp(&b.1).reverse());
+
     Ok(count_vec)
 }
